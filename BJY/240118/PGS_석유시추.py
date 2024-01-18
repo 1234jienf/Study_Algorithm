e7dd405887e57ac15b571456c4bdebfab8be7d98
백지윤 <wendy6992@naver.com>
2024-01-18 16:54:28 +0900

    $✅MOD: 240118(PGS_석유시추) 수정

--- BJY/240118/PGS_석유시추.py
+++ BJY/240118/PGS_석유시추.py
@@ -1,45 +1,45 @@
 # 문제
 ## 세로길이가 n, 가로길이가 m인 격자 모양의 땅 속에서 석유가 발견
-from collections import deque
+# from collections import deque
 
-def solution(land):
-    width = len(land)
-    depth = len(land[0])
+# def solution(land):
+#     width = len(land)
+#     depth = len(land[0])
     
-    dx = [0,1,-1,0]
-    dy = [1,0,0,-1]
+#     dx = [0,1,-1,0]
+#     dy = [1,0,0,-1]
 
-    visited = [[0] * (width) for _ in range(depth)]
-    result = [0 for _ in range(width)]
-    def bfs(cx,cy):
-        min_x, max_x = depth,0
-        num = 0
-        visited[cx][cy] = 1
-        q = deque()
-        q.append((cx,cy))
-        while q:
-            cx,cy = q.popleft()
-            min_x = min(min_x, cx)
-            max_x = max(max_x, cx)
-            num += 1
-            for i in range(4):
-                nx = cx + dx[i]
-                ny = cy + dy[i]
-                if 0 <= nx  < width and  0 <= nx < depth and not visited[nx][ny] and land[nx][ny]:
-                    visited[nx][ny] = 1
-                    q.append((nx,ny))
+#     visited = [[0] * (width) for _ in range(depth)]
+#     result = [0 for _ in range(width)]
+#     def bfs(cx,cy):
+#         min_x, max_x = depth,0
+#         num = 0
+#         visited[cx][cy] = 1
+#         q = deque()
+#         q.append((cx,cy))
+#         while q:
+#             cx,cy = q.popleft()
+#             min_x = min(min_x, cx)
+#             max_x = max(max_x, cx)
+#             num += 1
+#             for i in range(4):
+#                 nx = cx + dx[i]
+#                 ny = cy + dy[i]
+#                 if 0 <= nx  < width and  0 <= nx < depth and not visited[nx][ny] and land[nx][ny]:
+#                     visited[nx][ny] = 1
+#                     q.append((nx,ny))
                     
-        for i in range(min_x, max_x+1):
-            result[i] += num
+#         for i in range(min_x, max_x+1):
+#             result[i] += num
 
 
-    for i in range(width):
-        for j in range(depth):
-            if land[i][j] == 1 and not visited[i][j]:
-                bfs(i,j) 
+#     for i in range(width):
+#         for j in range(depth):
+#             if land[i][j] == 1 and not visited[i][j]:
+#                 bfs(i,j) 
                 
-    answer = max(result)
-    return answer
+#     answer = max(result)
+#     return answer
 
 
 from collections import deque
