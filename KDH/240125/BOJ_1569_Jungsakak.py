6534bfe1f556bd4669ec8974b1456c70c65e6611
KDH <silverstuff@naver.com>
2024-02-06 09:18:13 +0900

    ➕ADD: 2400125(BOJ_21569,9205,12849,PGS_성격유형검사하기) 추가

--- KDH/240125/BOJ_1569_Jungsakak.py
+++ KDH/240125/BOJ_1569_Jungsakak.py
@@ -1,44 +1,91 @@
+# N = int(input())
+# jum_list = []
+# for _ in range(N):
+#     X,Y = map(int, input().split())
+#     jum_list.append((X,Y))
+
+
+
+# plus = 0
+# minus = 0
+# boom = 0
+# jum_list.sort()
+
+# first_x = jum_list[0][0]
+# first_y = jum_list[0][1]
+# last_x = jum_list[-1][0]
+# last_y = jum_list[-1][1]
+
+# if first_y > last_y:
+#     for i in range(1,N):
+#         # x좌표가 시작과 끝점 사이에 있는경우
+#         if first_x < jum_list[i][0] < last_x:
+#             # y좌표가 둘 사이면
+#             if first_y < jum_list[i][1] < last_y:
+#                 boom = 1
+            
+            
+#         elif first_y    
+
+# elif first_y < last_y:
+#     pass
+
+# # 처음점과 마지막 점의 y좌표가 같을 때
+# else:
+#     pass
+
+# if boom == 0:
+#     ans = last_x - first_x
+# else:
+#     ans = -1
+# print(ans)
+
+#---------------------------------------
+
+
+from collections import deque
+
 N = int(input())
-jum_list = []
-for _ in range(N):
+Jum = deque()
+min_X = 9999999999
+max_X = -999999999
+min_Y = 999999999
+max_Y = -99999999999
+flag = 1
+for i in range(N):
     X,Y = map(int, input().split())
-    jum_list.append((X,Y))
-
-
-
-plus = 0
-minus = 0
-boom = 0
-jum_list.sort()
-
-first_x = jum_list[0][0]
-first_y = jum_list[0][1]
-last_x = jum_list[-1][0]
-last_y = jum_list[-1][1]
-
-if first_y > last_y:
-    for i in range(1,N):
-        # x좌표가 시작과 끝점 사이에 있는경우
-        if first_x < jum_list[i][0] < last_x:
-            # y좌표가 둘 사이면
-            if first_y < jum_list[i][1] < last_y:
-                boom = 1
-            
-            
-        elif first_y    
-
-elif first_y < last_y:
-    pass
-
-# 처음점과 마지막 점의 y좌표가 같을 때
+    if X < min_X:
+        min_X = X
+    elif X > max_X:
+        max_X = X
+    if Y < min_Y:
+        min_Y = Y
+    elif Y > max_Y:
+        max_Y = Y
+    Jum.append((X,Y))
+# print(max_X,min_X,max_Y,min_Y)
+while Jum:
+    now = Jum.popleft()
+    if now[0] != min_X and now[0] != max_X:        
+        if min_Y < now[1] < max_Y:
+            flag = 0
+        if max_X - min_X != max_Y - min_Y:
+            flag = 0
+        else:
+            continue
+    else:
+        continue
+    
+if flag:
+    print(max((max_X-min_X,max_Y-min_Y)))
 else:
-    pass
+    print(-1)
 
-if boom == 0:
-    ans = last_x - first_x
-else:
-    ans = -1
-print(ans)
+# --------------------------------------------
+
+
+
+
         
 
 
