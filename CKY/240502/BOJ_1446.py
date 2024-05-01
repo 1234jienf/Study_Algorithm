e6c3d16bbfe458e1ae785f1f15793f425a1f0fc2
choikeunyoung <ought119@gmail.com>
2024-05-02 02:09:34 +0900

    BOJ_1446.py 푸는중

--- CKY/240502/BOJ_1446.py
+++ CKY/240502/BOJ_1446.py
@@ -9,12 +9,27 @@ import heapq
 N, D = map(int,input().split())
 
 road = [[] for _ in range(10001)]
-road_weight = [ [0] * 10001 for _ in range(10001) ]
+road_weight = [[10**9,i] for i in range(N+1)]
 start_road = []
 
 for _ in range(N):
     S, E, W = map(int,input().split())
-    heapq.heappush(road[S],(W,E))
-    start_road.append(S)
+    if E <= D:
+        road[S].append((W,E))
 
-for 
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    road_weight[start][0] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if road_weight[now][0] < dist:
+            continue
+
+        for i in road[now]:
+            cost = dist + i[1]
+            if cost < road_weight[i[0]][0]:
+                road_weight[i[0]][0] = cost
+                road_weight[i[0]][0] = now
+                
