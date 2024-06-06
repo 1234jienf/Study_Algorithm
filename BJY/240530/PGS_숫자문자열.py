fd2be3f031dce87c65a2f581df3f27c78939f06e
백지윤 <wendy6992@naver.com>
2024-06-06 21:34:55 +0900

    ADD: 240530(BOJ_2174) 추가

--- BJY/240530/PGS_숫자문자열.py
+++ BJY/240530/PGS_숫자문자열.py
@@ -40,3 +40,42 @@
 # "23four5six7"	234567
 # "2three45sixseven"	234567
 # "123"	123
+
+
+# 0	zero
+# 1	one
+# 2	two
+# 3	three
+# 4	four
+# 5	five
+# 6	six
+# 7	seven
+# 8	eight
+# 9	nine
+
+def solution(s):
+    dict = {
+        'zero': '0',
+        'one' : '1',
+        'two' : '2',
+        'three' : '3',
+        'four' : '4',
+        'five' : '5',
+        'six' : '6',
+        'seven' : '7',
+        'eight' : '8',
+        'nine' : '9'
+            }
+    answer = ''
+    word = ''
+    for i in range(len(s)):
+        if s[i].isdigit():
+             answer += s[i] 
+        else:
+          word += s[i]
+          if word in list(dict.keys()):
+            answer += dict[word]
+            word = ''
+          else:
+            continue
+    return int(answer)
