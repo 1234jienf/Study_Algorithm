f3bd08903cd780472afc3193e7c4190db39ed236
choikeunyoung <ought119@gmail.com>
2024-07-12 04:12:47 +0900

    MOD: BOJ_2212.py

--- CKY/240704/BOJ_2212.py
+++ CKY/240704/BOJ_2212.py
@@ -1,3 +1,17 @@
 N = int(input())
 K = int(input())
 num_list = list(map(int,input().split()))
+
+if K >= N:
+    print(0)
+else:
+    cnt = 0
+    num_list.sort()
+    answer_list = []
+    for i in range(1,N):
+        if num_list[i] != num_list[i-1]:
+            answer_list.append(num_list[i]-num_list[i-1])
+        else:
+            cnt += 1
+    answer_list.sort()
+    print(sum(answer_list[:N-cnt-K]))
