b87e8d07b6993e1e2832740e22ac5b2b9319850d
KTaeGyu <xorb269@naver.com>
2024-09-28 02:22:27 +0900

    ADD: 240913 CT_magic_tree

--- KTG/240913/CT_magic_forest.py
+++ KTG/240913/CT_magic_forest.py
@@ -5,26 +5,83 @@
 import sys
 sys.stdin = open('./KTG/240913/CT_magic_forest.txt', 'r')
 
+R, C, K = map(int, input().split())
+unit = [list(map(int, input().split())) for _ in range(K)]
+arr = [[1] + [0] * C + [1] for _ in range(R + 3)] + [[1] * (C + 2)]
+exit_set = set()
 
-def canDown():
-  global pos
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
+
+
+def bfs(si, sj):
+  q = []
+  v = [[0] * (C+2) for _ in range(R+4)]
+  mx_i = 0
+  
+  q.append((si, sj))
+  v[si][sj] = 1
+  
+  while q:
+    ci, cj = q.pop(0)
+    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+      ni, nj = ci+di, cj+dj
+      if v[ni][nj] == 0 and (arr[ci][cj] == arr[ni][nj] or ((ci, cj) in exit_set and arr[ni][nj] > 1)):
+        q.append((ni, nj))
+        v[ni][nj] = 1
+        mx_i = max(mx_i, ni)
+  
+  return mx_i - 2
+
+
+ans = 0
+num = 2
+for cj, dr in unit:
+  ci = 1
+  while True:
+    if arr[ci+1][cj-1] + arr[ci+2][cj] + arr[ci+1][cj+1] == 0:
+      ci += 1
+    elif arr[ci-1][cj-1] + arr[ci][cj-2] + arr[ci+1][cj-1] + arr[ci+1][cj-2] + arr[ci+2][cj-1] == 0:
+      ci += 1
+      cj -= 1
+      dr = (dr-1)%4
+    elif arr[ci-1][cj+1] + arr[ci][cj+2] + arr[ci+1][cj+1] + arr[ci+1][cj+2] + arr[ci+2][cj+1] == 0:
+      ci += 1
+      cj += 1
+      dr = (dr+1)%4
+    else:
+      break
+    
+  if ci < 4:
+    arr = [[1] + [0] * C + [1] for _ in range(R + 3)] + [[1] * (C + 2)]
+    exit_set = set()
+    num = 2
+  else:
+    arr[ci+1][cj]=arr[ci-1][cj]=num
+    arr[ci][cj-1:cj+2]=[num]*3
+    num += 1
+
+    exit_set.add((ci+di[dr], cj+dj[dr]))
+    
+    ans += bfs(ci, cj)
+    
+print(ans)
+    
+"""
+def canMove(dir: list[list[int, int]], turn: int):
+  global pos, d
   y, x = pos;
-  for dy, dx in down:
+  for dy, dx in dir:
     ny, nx = y + dy, x + dx
-    if board[ny][nx] == 1:
+    if ny <= 0 or C + 2 < ny or nx <= 0 or R + 2 < nx:
+      break
+    if board[ny][nx] > 0:
       break
   else:
     pos = [y + 1, x]
+    d = (d + turn) % 4
     return True
   return False
-
-
-def canLeft():
-  return
-
-
-def canRight():
-  return
 
 
 direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
@@ -39,19 +96,23 @@ for k in range(K):
   c, d = map(int, input().split())
   pos = [0, c]
   while True:
-    if canDown():
+    if canMove(down, 0):
       continue
-    elif canLeft():
+    elif canMove(left, -1):
       continue
-    elif canRight():
+    elif canMove(right, 1):
       continue
     else:
       y, x = pos
-      board[y][x] = 1
-      for dy, dx in direction:
+      if 0 < y < R + 1:  
+        board[y][x] = 1
+        for dy, dx in direction:
+          ny, nx = y + dy, x + dx
+          board[ny][nx] = 1
+        dy, dx = direction[d]
         ny, nx = y + dy, x + dx
-        board[ny][nx] = 1
+        board[ny][nx] += 1
       break
-  print(board)
-  
-  
+    
+  pprint.pprint(board)
+"""
