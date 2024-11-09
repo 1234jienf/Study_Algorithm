114656f1363e821d7854b90fc93aa57c68cb2ab9
minji <3dlalswl3@gmail.com>
2024-11-09 12:36:21 +0900

    ✅MOD: 241102(PGS_상담원인원) 주석 수정

--- LMJ/241102/PGS_상담원인원.java
+++ LMJ/241102/PGS_상담원인원.java
@@ -28,7 +28,7 @@ class Solution {
         for(int i = 0; i < arr.length; i++) {
             int count = i + 1;  // 멘토 수
             PriorityQueue<Integer> endTimes = new PriorityQueue<>();
-            int totalTime = 0;
+            int totalTime = 0; // 총 대기 시간
             
             for(Integer[] time : list) {
                 int start = time[0];
@@ -52,7 +52,7 @@ class Solution {
         }
     }
     
-    // 가능한 멘토 조합 완전 탐색
+    // 가능한 멘토 조합 dfs 탐색
     // max: 현재 배정 가능한 남은 멘토 수
     // arr: 각 유형별, 멘토 수별 대기시간 배열
     // type: 현재 처리중인 상담 유형
@@ -60,9 +60,11 @@ class Solution {
         int minTotal = Integer.MAX_VALUE;
         for(int i = 0; i <= max; i++) {
           int value = arr[type][i];
+            // 마지막 유형이 아닌 경우 재귀
             if (type < arr.length - 1) {
                 value += dfs(max - i, arr, type + 1);
             }
+            // 최소 대기 시간 갱신
             minTotal = Math.min(minTotal, value);
         }
         return minTotal;
