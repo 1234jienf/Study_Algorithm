7056f56b4250f92f9464f44d5402ff20306c835b
백지윤 <wendy6992@naver.com>
2025-04-07 15:02:24 +0900

    ADD: 250406(BOJ_10775, 4386, 1920) 추가

---
+++ BJY/250406/tempCodeRunnerFile.py
@@ -0,0 +1,23 @@
+N = int(input())
+N_lst = list(map(int,input().split()))
+M = int(input())
+M_lst = list(map(int,input().split()))
+
+N_lst.sort()
+
+
+for num in M_lst:
+  ## idx값으로
+  left,right = 0, N-1
+  found = False
+  while left <= right:
+    mid = ( left + right )//2
+    if N_lst[mid] == num:
+      found == True
+      break
+    else:
+      if N_lst[mid] > num:
+        right = mid - 1
+      else:
+        left = mid + 1
+  print(1 if found else 0)
